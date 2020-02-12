c3ccaf7408ede4b92a6970e4ab7a6261d7ac8f10
MBtsky <9250499222@mail.ru>
2020-02-12 19:55:11 +0300

    feat(environment): sass added, jsx support, eslint not yet

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,34 @@ module.exports = {
           // 'extract-loader',
         ],
       },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      },
+      {
+        test: /\.sass$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      },
     ]
   }
 }
