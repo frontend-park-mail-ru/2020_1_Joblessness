8f58f30a4057a438e5e209c237338e22594c8535
MBtsky <9250499222@mail.ru>
2020-02-22 16:33:56 +0300

    docs(uuid): JsDoc added docs(withNetwork): JsDoc added

--- src/ulils/uuid.js
+++ src/ulils/uuid.js
@@ -1,3 +1,7 @@
+/**
+ * Generates unique string (suitable for dom id)
+ * @returns {string}
+ */
 export const uuid = () =>
     `_${Math.random()
         .toString(36)
--- src/ulils/withNetwork.js
+++ src/ulils/withNetwork.js
@@ -1,5 +1,21 @@
-const withNetwork = (url, WrappedComponent, propName, defaultProps = {}) => {
+/**
+ *
+ * @param {string} url - url to fetch
+ * @param {Page} WrappedComponent - Component to wrap
+ * @param {string} propName - key to store response in Component.props[key]
+ * @param {Object} defaultProps - default value - used before response is received (Optimistic update)
+ * @returns {Page} - Wrapped Component
+ */
+import {validateString} from "./index";
 
+const withNetwork = (url, WrappedComponent, propName="fetched", defaultProps = {}) => {
+    validateString(url, "url", true);
+    validateString(propName, "propName", true);
+    if(!WrappedComponent.isPageComponent) {
+        throw new Error(`
+        Expected Page Component as WrappedComponent
+        `)
+    }
     return class extends WrappedComponent {
         constructor(...args) {
             super(args);
