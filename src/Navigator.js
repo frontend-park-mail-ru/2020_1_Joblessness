07cac4c2c4bf2b5749b8b7803d72908d40483442
MBtsky <9250499222@mail.ru>
2020-02-22 16:18:55 +0300

    docs(Navigator): JsDoc updated docs(Page): JsDoc updated

--- src/Navigator.js
+++ src/Navigator.js
@@ -4,15 +4,32 @@ import {getBus} from "./ulils/getBus";
 import {validateString} from './ulils';
 import {hideAll} from "./ulils/showPage";
 
+/**
+ * Navigator
+ * @class
+ * Осуществляет переключение между страницами
+ * На вход получает объект routes,
+ * в котором ключем является data-page элемента,
+ * а значением - название конструктора страницы, находящейся в BUS
+ */
 class Navigator {
     /**
      * Все страницы создаются здесь и помещаются в глобальный объект Bus
      */
     constructor(routes = {}) {
         this.addNavEvents();
+        if( typeof routes !== 'object' || routes === null ) {
+            throw new Error(`
+            Expected object as field routes. Received ${routes}!`)
+        }
         this.routes = routes
     }
 
+    /**
+     * Open requested page in navigator
+     * @public
+     * @param {string} pageName
+     */
     showPage = (pageName) => {
         hideAll();
         if (typeof this.routes[pageName] === 'string') {
@@ -23,9 +40,9 @@ class Navigator {
             getBus().pagesOnScreen?.[this.routes[pageName]]?.requestRender();
             window.history.replaceState({}, '', pageName);
         } else {
-            console.log(getBus().pagesOnScreen);
             getBus().pagesOnScreen?.NotFoundPage?.requestRender();
             window.history.replaceState({}, '', "404");
+            //@TODO Are there any cases when we should throw this error?
             // throw new Error(`
             // Unable to find ${pageName} in routes.
             // You must have forgotten to add it to Application Routes!`);
@@ -33,9 +50,13 @@ class Navigator {
     };
 
     // имя класса самого элемента
-    domName() {
-        return 'nav-bar'
-    }
+    /**
+     * @deprecated
+     * Navigator на данный момент не привязывается к элементу на странице.
+     */
+    // domName() {
+    //     return 'nav-bar'
+    // }
 
     /**
      * Обработка нажатий на все ссылки с целью перехода на другую страницу
@@ -53,11 +74,23 @@ class Navigator {
 }
 
 
+/**
+ * Automatically adds pages to BUS and creates navigator with desired pages.
+ * Useful shortcut.
+ * @param {Page} pages - Page components to be shown
+ * @param {Object} routes - Page components to be displayed at
+ * @param {string} container - dom element id to insert elements
+ * @returns {Navigator}
+ */
 const withBus = (pages, routes, container) => {
+    //@TODO убрать привязку к BUS.
     validateString(container, "container", true);
-    //@TODO validate pages
     let pagesToShow = {};
     for (let page of Object.keys(pages)) {
+        if(!pages[page].isPageComponent) {
+            throw new Error(`
+            Expected Page Component as value in route ${page}. Received ${pages[pages]}`)
+        }
         pagesToShow[page] = new pages[page](container)
     }
 
--- src/Page.js
+++ src/Page.js
@@ -1,6 +1,8 @@
 "use strict";
-import { uuid } from './ulils'
+import {uuid} from './ulils'
+
 /**
+ * @class
  * Page - класс от которого наследуются страницы
  * описывает общие методы и реализует метод создания в DOM бокса под страницу
  * наверное можно заменить на миксин createDomBox
@@ -11,6 +13,7 @@ export class Page {
     componentDidMount = null;
     componentWillMount = null;
     static isPageComponent = true;
+
     /**
      * Геттер dom-элемента старицы
      * @returns {Element}
@@ -40,8 +43,7 @@ export class Page {
      * @returns {string}
      */
     domName() {
-        if( typeof this.name === "function" ) {
-            // console.error(`Names are not supported anymore!`);
+        if (typeof this.name === "function") {
             return `${this.name()}`;
         }
         return this.__name;
@@ -51,19 +53,19 @@ export class Page {
      * Спрятать страницу
      */
     hidePage() {
-        this.getDomElem().style.display = "none"
+        this.getDomElem().style.display = "none";
         this.getDomElem().hidden = true;
     }
 
     /**
      * Создать блок страницы и поместить его в контейнер
-     * @param domName - имя создаваемого блока
+     * @param domName - имя-класс создаваемого блока
      * @returns {HTMLDivElement}
      */
     createDomBox(domName) {
-        if( !this.domBox ) {
+        if (!this.domBox) {
             this.domBox = document.createElement("div");
-            this.domBox.className = domName;
+            this.domBox.className = domName; //@TODO should replace with id?
             // все страницы по умолчанию скрыты
             this.domBox.hidden = true;
             this.container.appendChild(this.domBox);
@@ -72,10 +74,20 @@ export class Page {
 
     }
 
-    render() {}
+    /**
+     * Возвращает строку с содержимым страницы.
+     * @WARNING не использовать напрямую
+     */
+    render() {
+        throw new Error(`
+        Method render must be overwritten!
+        `)
+    };
 
     /**
-     * Очистить страницу к начальному состоянию
+     * Очистить страницу к начальному состоянию.
+     * @deprecated
+     * Более не нужен, так как это выполняется при перерисовке страницы через requestRender()
      */
     clearPage() {
         console.error(`
@@ -99,7 +111,7 @@ export class Page {
      */
     showPage() {
         const dom = this.getDomElem();
-        if( !dom ) {
+        if (!dom) {
             throw new Error(`
             Render function must return string!`
             )
@@ -109,9 +121,15 @@ export class Page {
     }
 
     requestRender() {
+        if (typeof this.render !== "function") {
+            throw new Error(`
+            Method render is reserved by Page Component and
+            must be overwritten by function with return
+            value of type string!`);
+        }
         this.componentWillMount && this.componentWillMount();
         const toShow = this.render();
-        if( toShow ) {
+        if (toShow) {
             this.domBox.innerHTML = toShow;
         } else {
             console.error(`
