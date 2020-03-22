d28285f8021fa56da7f3b14fa0d9199222d89f4b
MBtsky <9250499222@mail.ru>
2020-03-23 01:19:03 +0300

    feat(create summary): education implementation in progress

--- src/views/NewCreateSummaryPage/showEducation/EducationItem/index.js
+++ src/views/NewCreateSummaryPage/showEducation/EducationItem/index.js
@@ -10,8 +10,22 @@ class EducationItem extends Page {
    * @return{string}
    */
   render() {
-    console.log(this.props)
     return template(this.props);
+  }
+  componentDidMount() {
+    super.componentDidMount?.();
+    if(!this._wasMounted) {
+      const e = document.querySelector(this.container).parentNode;
+      e.classList.add('placing');
+      setTimeout(
+        () => e.classList.remove('placing'), 500
+      );
+      e.classList.add('removing');
+      setTimeout(
+        () => e.classList.remove('removing'), 1
+      );
+      this._wasMounted = true
+    }
   }
 }
 
