77a49aaa980771bcd9fdc3570af06243dd41cd3f
MBtsky <9250499222@mail.ru>
2020-04-27 14:19:11 +0300

    prodv3

--- src/ws.js
+++ src/ws.js
@@ -3,8 +3,6 @@ const wsDomain = DOMAIN.replace('http://', 'ws://');
 export default () => {
   const ws = new WebSocket(`${wsDomain}/api/chat`);
   ws.onopen = (e) => {
-    ws.send('1');
-    console.log(e, 'open')
   };
   ws.onmessage = (...msg) => {
     try {
