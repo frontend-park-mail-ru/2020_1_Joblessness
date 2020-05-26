fe4c4b188c4352854cc1408fe57c0061266663f4
MBtsky <9250499222@mail.ru>
2020-05-26 12:17:09 +0300

    current version

--- src/views/SearchPage/Item/index.js
+++ src/views/SearchPage/Item/index.js
@@ -1,9 +1,6 @@
 import {Page} from '../../../Page';
 import template from './index.pug';
 import './style.sass';
-import {
-  playMountAnimation,
-} from '../../OrganizationPage/OrganizationVacancies/addAnimations';
 
 class Item extends Page {
   render() {
@@ -15,8 +12,10 @@ class Item extends Page {
     const parent = document.querySelector(this.container);
     if (!this._wasMount) {
       this._wasMount = true;
-      parent.classList.add('placing')
+      parent.classList.add('placing');
+      setTimeout(() => parent.classList.remove('start'), 100);
     } else {
+      setTimeout(() => parent.classList.remove('start'), 10);
       if (parent) {
         parent.style.transitionDuration = '0';
       }
